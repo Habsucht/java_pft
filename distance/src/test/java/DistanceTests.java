74a4c27b9ed7a9052af095ca6baa657f89ed1fc2
Aleksey K <aleksey.aka.gremlin@gmail.com>
2016-05-31 21:09:27 +0300

    My first test

---
+++ distance/src/test/java/DistanceTests.java
@@ -0,0 +1,14 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DistanceTests {
+
+    @Test
+    public void testDistance() {
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(1, 2);
+
+        Assert.assertEquals(Point.distance(point1, point2), 1.0);
+
+    }
+}
