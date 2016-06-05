07a108f3aad05289dd6534c923bdaf02e5cce704
Aleksey K <aleksey.aka.gremlin@gmail.com>
2016-06-05 08:17:57 +0300

    Create GroupModificationTests

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -37,4 +37,12 @@ public class GroupHelper extends BaseHelper {
     public void deleteGroup() {
         click(By.name("delete"));
     }
+
+    public void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
 }
---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.data.GroupData;
+
+public class GroupModificationTests extends BaseTests{
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1_mod", "info1_mod", "info1_mod"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
