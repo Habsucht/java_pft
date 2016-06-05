9820214646421f64bfb8e7106f1da50aa2b8f073
Aleksey K <aleksey.aka.gremlin@gmail.com>
2016-06-05 08:49:12 +0300

    Create ContactDeletionTests

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.Test;
+
 public class ContactDeletionTests extends BaseTests {
+
+    @Test
+    public void testContactDeletion() {
+        app.getContactHelper().selectContact();
+        app.getContactHelper().deleteContact();
+        app.getBaseHelper().alert();
+    }
+
 
 }
