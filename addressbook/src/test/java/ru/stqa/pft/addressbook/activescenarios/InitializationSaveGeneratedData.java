97e0c23fe2ec914825e91d0c3d350664cee07472
Habsucht <aleksey.aka.gremlin@gmail.com>
2016-07-14 17:24:34 +0300

    Update class InitializationSaveGeneratedData

--- addressbook/src/test/java/ru/stqa/pft/addressbook/activescenarios/InitializationSaveGeneratedData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/activescenarios/InitializationSaveGeneratedData.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.addressbook.activescenarios;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.data.ContactData;
 import ru.stqa.pft.addressbook.data.GroupData;
 
 import java.io.File;
@@ -8,18 +12,44 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class InitializationSaveGeneratedData {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
 
-        saveGroups(count, file);
+    @Parameter(names = "-c", description = "count")
+    public int count;
+
+    @Parameter(names = "-f", description = "file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        InitializationSaveGeneratedData initializationSaveGeneratedData = new InitializationSaveGeneratedData();
+        JCommander jCommander = new JCommander(initializationSaveGeneratedData);
+
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        initializationSaveGeneratedData.run();
     }
+
+    private void run() throws IOException {
+        saveGroups(count, new File(file));
+    }
+
 
     public static void saveGroups(int count, File file) throws IOException {
         Writer writer = new FileWriter(file);
 
         for (int i = 0; i < count; i++) {
             writer.write(new GroupData().toString()); }
+        writer.close();
+    }
+
+    public static void saveContacts(int count, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+
+        for (int i = 0; i < count; i++) {
+            writer.write(new ContactData().toString()); }
         writer.close();
     }
 }
