37968ba4eab680f0bdd834be7f93857bd69abe86
Habsucht <aleksey.aka.gremlin@gmail.com>
2016-07-20 12:24:44 +0300

    Update test class MysqlHibernateConnectionTests

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/bd/MysqlHibernateConnectionTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/bd/MysqlHibernateConnectionTests.java
@@ -21,12 +21,15 @@ public class MysqlHibernateConnectionTests {
                 .configure() // configures settings from hibernate.cfg.xml
                 .build();
         try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            sessionFactory = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
         }
         catch (Exception e) {
             e.printStackTrace();
             // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
+            StandardServiceRegistryBuilder
+                    .destroy(registry);
         }
     }
 
@@ -34,7 +37,7 @@ public class MysqlHibernateConnectionTests {
     public void testDbConnectionForHibernate() {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        List<GroupData> result = session.createQuery( "from GroupData" ).list();
+        List<GroupData> result = session.createQuery("from GroupData").list();
 
         for ( GroupData group : result ) { System.out.println(group); }
 
